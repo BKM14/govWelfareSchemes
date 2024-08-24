71c3e9533693cb8a833bec3cad30fb0566383ade
Balaji Krishnamurthy <balajikrishnamurthy2004@gmail.com>
2024-08-24 16:24:34 +0530

    decrease navbar font size

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import { NavComponent } from "./NavComponent"
 
 export function NavBar({NavData}) {
-    return <div className="flex flex-wrap justify-center md:justify-between items-center text-sm md:text-md lg:text-xl text-center border-2 bg-slate-100">
+    return <div className="flex flex-wrap justify-center md:justify-between items-center text-sm md:text-lg  text-center border-2 bg-slate-100">
         {NavData.map((item, index) => {
             return <NavComponent label={item.name} to={item.address} key={index}/>
         })}
