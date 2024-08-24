fde4677072d5f145ac1064a19654f9c39c06d153
Balaji Krishnamurthy <balajikrishnamurthy2004@gmail.com>
2024-08-24 10:11:56 +0530

    fix config files

--- frontend/src/components/SubPage.jsx
+++ frontend/src/components/SubPage.jsx
@@ -1,5 +1,5 @@
 export function SubPage({data}) {
-    return <div className="m-10 font-montserrat">
+    return <div className="md:m-10 m-3 font-montserrat">
         <p className="font-bold text-lg md:text-xl lg:text-3xl mt-3 text-center">{data.heading}</p>
         <div className="p-10 font-poppins mb-20 text-md md:text-lg lg:text-xl">{data.content}</div>
     </div>
